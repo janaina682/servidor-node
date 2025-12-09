ea9fb96e4db4f52cefb9342be54e2a75e7058c3b
Janaina Jucelly Nascimento do Vale <jucellyjanaina1@gmail.com>
2025-12-09 16:59:51 -0300

    Conectando a um banco de dados postgres usando Sequelize

---
+++ conexaoBD.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";// importa o Sequelize do pacote sequelize que é usado para conectar ao banco de dados
+
+const sequelize = new Sequelize(
+    'postgres', // nome do banco
+    'postgres',       // usuário
+    '172471',         // senha
+    {
+        host: 'localhost', // host do banco de dados
+        dialect: 'postgres',// tipo do banco de dados driver
+        port: 5432,// porta do banco de dados
+    }
+)
+
+// Função para testar a conexão com o banco de dados
+async function connect() {
+    try {
+        await sequelize.authenticate();
+    //sequelize.authenticate() tenta autenticar a conexão com o banco de dados
+        console.log('Conecção estabelecida com sucesso.');
+    } catch (error) {
+        console.error('Não foi possível conectar ao banco de dados:', error);
+    }
+}
+connect()
